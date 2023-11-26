8035d4791e02e4911904c8f3b62f5c6a74b483d5
mohamuhsin <muhsinohoho@outlook.com>
2023-11-26 03:29:00 +0300

    changed the state of setIsEditing dynamically using setIsEditing(!isEditing)

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,13 +4,13 @@ export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
-    setIsEditing(true);
+    setIsEditing(!isEditing);
   }
 
   let playerName = <span className="player-name">{name}</span>;
   let btnCaption = "Edit";
   if (isEditing) {
-    playerName = <input type="text" required />;
+    playerName = <input type="text" required value={name} />;
     btnCaption = "Save";
   }
 
